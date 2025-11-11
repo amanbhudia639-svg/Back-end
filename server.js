ae9bf964cc8a085c966c39401d982db57294f3c8
Aman <amanbhudia639@gmail.com>
2025-11-11 19:57:31 +0000

    POST sends the orders to database collection order

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 const uri = process.env.MONGO_URI;
 
+// Enable CORS so Vue can talk to Express
+app.use(cors());
+app.use(express.json());
+
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
@@ -22,12 +26,13 @@ async function connectDB() {
 }
 
 connectDB();
+const db = client.db("ProductList");
+const collection = db.collection("products");
+const orders = db.collection("ORDERS");
 
 
 app.get('/lessons', async (req, res) => {
   try {
-    const db = client.db("ProductList");
-    const collection = db.collection("products");
 
     const products = await collection.find({}).toArray();
 
@@ -41,6 +46,33 @@ app.get('/lessons', async (req, res) => {
     console.error("Database error:", err);
     res.status(500).json({ message: "Database error" });
   }
+});
+
+app.post('/Order', async (req, res) => {
+  const { Name, phone, productid, pname, quantity} = req.body;
+
+  const namePattern = /^[A-Za-z\s]+$/;
+  if (!namePattern.test(Name)) {
+    return res.status(400).json({ error: "Please enter letters." });
+  }
+  
+  const PhonePattern = /^[0-9]+$/;
+  if (!PhonePattern.test(phone)) {
+    return res.status(400).json({ error: "Please enter numbers." });
+  }
+  
+  try {
+
+    const userinput = { Name, phone, productid, pname, quantity };
+    await orders.insertOne(userinput);
+
+    res.status(201).json({ message: "Order successfully placed!"});
+  } catch (error) {
+    console.error("Error saving order:", error);
+    res.status(500).json({ error: "Error" });
+    
+  }
+
 });
 
 

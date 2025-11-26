725eb9adcdbd4daa96f1f81f95aca761e642c02d
Aman <amanbhudia639@gmail.com>
2025-11-26 14:11:05 +0000

    comments have been added, Logger systenm to test middleware

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Back-end
+
+code was made with node js and express js 
+
+NO need to add a static middleware file as my frontend dose not contain any images and only uses icons 
+
+## List of Request 
+GET (used to retrieve leasons and for searching)
+POST (used for sending orders that peole have bought on the website)
+PUT (used to adjust the quantity for the product once the order is processed and add to the database)
+
+# List of modules 
+body-parser
+cors
+express
+mongodb
+
--- server.js
+++ server.js
@@ -1,61 +1,72 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { MongoClient } = require('mongodb');
+require('dotenv').config(); // Load variables from the .env file
+const express = require('express'); // Imports Express framework
+const cors = require('cors'); // Imports CORS middleware
+const { MongoClient } = require('mongodb'); // Imports mongoDB client 
 
-const app = express();
-// Enable CORS so Vue can talk to Express
-app.use(cors());
-app.use(express.json());
-const uri = process.env.MONGO_URI;
+const app = express(); // Creates an exppress app 
 
-// Enable CORS so Vue can talk to Express
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Enable CORS so Vue can talk to Express
+app.use(express.json()); // Allows Express to work with json request
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
+// LOGGING middleware
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp}`); // Log timestamp
+  console.log(`${req.method}${req.originalUrl}`); // Log HTTP method + URL
+  console.log();
+  next(); // Continues request
+});
 
+// NO need to add a static middleware file as my frontend dose not contain any images and only uses icons 
+
+const uri = process.env.MONGO_URI; // Loads MongoDB connection from enviroment 
+
+const client = new MongoClient(uri); // Create a new MongoClient
+
+// connect to MongoDB
 async function connectDB() {
   try {
-    await client.connect();
-    console.log('✅ Connected to MongoDB Atlas');
+    await client.connect(); // attempts conection 
+    console.log('Connected to MongoDB Atlas');
   } catch (error) {
-    console.error('❌ MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error);
   }
 }
 
 connectDB();
-const db = client.db("ProductList");
-const collection = db.collection("products");
-const orders = db.collection("ORDERS");
+const db = client.db("ProductList"); // Select database
+const collection = db.collection("products"); // Select products collection inside the productlist database
+const orders = db.collection("ORDERS"); // Select ORDERS collection inside the productlist database
 
-
+//route is used to fetch all the products
 app.get('/lessons', async (req, res) => {
   try {
 
-    const products = await collection.find({}).toArray();
+    const products = await collection.find({}).toArray(); // Retrieves all products
 
     if (products.length === 0) {
-      res.status(404).json({ message: "No products found" });
+      res.status(404).json({ message: "No products found" }); // no products found 
     } else {
-      res.json(products);
+      res.json(products); // Returns product list 
     }
 
-  } catch (err) {
-    console.error("Database error:", err);
-    res.status(500).json({ message: "Database error" });
+  } catch (error) {
+    console.error("Database error:", error);
+    res.status(500).json({ message: "Database error" }); // if database fails
   }
 });
 
+//route is used to post the order to the database (ORDERS collection)
 app.post('/Order', async (req, res) => {
-  const { Name, phone, productid, pname, quantity} = req.body;
+  const { Name, phone, productid, pname, quantity, total} = req.body;
 
+  //Validates the names (only letters and spaces)
   const namePattern = /^[A-Za-z\s]+$/;
   if (!namePattern.test(Name)) {
     return res.status(400).json({ error: "Please enter letters." });
   }
   
+  //Validates the phone (only numbers)
   const PhonePattern = /^[0-9]+$/;
   if (!PhonePattern.test(phone)) {
     return res.status(400).json({ error: "Please enter numbers." });
@@ -63,8 +74,8 @@ app.post('/Order', async (req, res) => {
   
   try {
 
-    const userinput = { Name, phone, productid, pname, quantity };
-    await orders.insertOne(userinput);
+    const userinput = { Name, phone, productid, pname, quantity, total};
+    await orders.insertOne(userinput); // Inserts the order into the database 
 
     res.status(201).json({ message: "Order successfully placed!"});
   } catch (error) {
@@ -75,52 +86,53 @@ app.post('/Order', async (req, res) => {
 
 });
 
+// route that searchs for the products
 app.get('/lessons/search', async (req, res) => {
-    const { query } = req.query;
+    const { query } = req.query; //Get the search query from url 
 
     if (!query) {
         return res.status(400).json({ message: "Query is required for searching." });
     }
 
-    const numericQuery = Number(query);
-    const isNumeric = !isNaN(numericQuery);
+    const numericQuery = Number(query); // converts query to number
+    const isNumeric = !isNaN(numericQuery); // checks if the value is not a number 
 
     try {
 
-        const Conditions = [];
+        const Conditions = []; // array conating search conditions
 
-        Conditions.push({ pname: { $regex: query, $options: 'i' }});
-        Conditions.push({ location: { $regex: query, $options: 'i' }});
+        Conditions.push({ pname: { $regex: query, $options: 'i' }}); // Search name of the product (case insensitive)
+        Conditions.push({ location: { $regex: query, $options: 'i' }}); // Search location of the product (case insensitive)
 
-        //price and quantity search
         if (isNumeric) {
-            Conditions.push({ price: numericQuery });
-            Conditions.push({ quantity: numericQuery });
+            Conditions.push({ price: numericQuery }); // search price
+            Conditions.push({ quantity: numericQuery }); // search quantity
         }
 
         const products = await collection.find({$or: Conditions }).toArray();
 
-        res.status(200).json({ products });
+        res.status(200).json({ products });// returns matching products
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).json({ message: "An error occurred while fetching users." });
     }
 });
 
+// route for adjusting quntity 
 app.put('/quantity', async(req, res) =>{
   try{
-    const { pname, quantity } = req.body;
+    const { pname, quantity } = req.body; // Extracts fields from request body
 
     const change = await collection.updateOne(
-      {pname: pname },
-      {$set: {quantity: quantity}}
+      {pname: pname }, // Finds product by name
+      {$set: {quantity: quantity}} // update quantity 
     );
 
     if(change.matchedCount === 0 ){
-      return res.status(404).jsonp({message: "Product not found"});
+      return res.status(404).json({message: "Product not found"}); // no matching products
     }
 
-    res.json({message: "Quantity updated", change});
+    res.json({message: "Quantity updated", change}); // successful update
   }catch (error){
     console.error(error);
     res.status(500).json({message: "Server error"});
@@ -129,8 +141,6 @@ app.put('/quantity', async(req, res) =>{
 });
 
 
-
-
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // start 
 
